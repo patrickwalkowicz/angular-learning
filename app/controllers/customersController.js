98baa347550ceb60abe44ff5a50cea2d58568065
Patrick Walkowicz <patrykwalkowicz@gmail.com>
2015-10-19 15:41:35 +0100

    Added values

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,10 +1,11 @@
 (function() {
     // Define controller function
-    var CustomersController = function($scope, customersFactory) {
+    var CustomersController = function($scope, customersFactory, appSettings) {
         // Defaults
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.customers = [];
+        $scope.appSettings = appSettings;
         
         function init() {
             $scope.customers = customersFactory.getCustomers();
@@ -18,7 +19,7 @@
         }
     }
     // Inject '$scope' to prevent minifiers from breaking the code
-    CustomersController.$inject = ['$scope', 'customersFactory'];
+    CustomersController.$inject = ['$scope', 'customersFactory', 'appSettings'];
     
     // Inject the function as module's controller
     angular.module('customersApp')          
